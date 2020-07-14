33bcdac75b76b7806e93c48055189fdc3e2addcc
anzhiruotai <1029454322@qq.com>
2020-07-14 15:15:26 +0800

    biginfo

--- MuduleWebEE/src/Big.java
+++ MuduleWebEE/src/Big.java
@@ -6,6 +6,25 @@ public class Big {
     public static void main(String[] args) {
         System.out.println("big");
         int a = 0;
+        getPrintln(a);
+
+
+
+        getPrintln(a);
+        try {
+            System.out.println(b);
+        } catch (Exception e) {
+            try {
+                e.printStackTrace();
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        }
+
+
+    }
+
+    private static void getPrintln(int a) {
         System.out.println(a);
     }
 }
